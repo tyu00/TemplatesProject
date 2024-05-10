3cf3c76f1a565cf42e2b1e10ee4260f1e48fe180
tyu00 <g.s.u.toshov02@yandex.com>
2024-05-10 20:40:57 +0500

    2

--- my_site/my_site/news/urls.py
+++ my_site/my_site/news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name= 'home'),
+    path('', index, name='home'),
     path('category/<int:category_id>', get_category, name='category'),
 ]
